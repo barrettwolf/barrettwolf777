de32e9e530c5c95ba07041a05f5a66d03948e8b9
Barre <barrettwolf@Barretts-MacBook-Pro.local>
2020-09-05 23:52:59 -0700

    Lab8 Commit

---
+++ Lab8War/Shuffle.hpp
@@ -0,0 +1,18 @@
+//
+//  Shuffle.hpp
+//  Lab8War
+//
+//  Created by Barrett Wolf on 11/4/19.
+//  Copyright © 2019 Barrett Wolf. All rights reserved.
+//
+
+#ifndef Shuffle_hpp
+#define Shuffle_hpp
+
+#include <iostream>
+#include "Card.hpp"
+using namespace std;
+
+void Shuffle(queue <Card>& q); //declaring the function that takes in a queue
+
+#endif
